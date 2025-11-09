59c21dc9886d7aa23da5b0501ea6193e6df6f829
Maria Luisa Saraiva <mlsg.eng24@uea.edu.br>
2025-11-09 18:33:16 -0400

    telas básicas da história do chefe

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,12 +6,24 @@ def cadastro_view(request):
         form = FormularioCadastroUsuario(request.POST)
         if form.is_valid():
             form.save() 
-            
-            return redirect('home') 
+            return redirect('home')
     else:
         form = FormularioCadastroUsuario()
     
     return render(request, 'usuarios/F_Tela_Cadastro.html', {'form': form})
 
+
 def historia_chefe(request):
-    return render(request, 'usuarios/chefe_historia.html')
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        resumo = request.POST.get('resumo')
+        receitas = request.POST.get('receitas').split(';')  # separa receitas por ;
+        contexto = {
+            'nome': nome,
+            'resumo': resumo,
+            'receitas': receitas
+        }
+        return render(request, 'usuarios/chefe_historia.html', contexto)
+    else:
+        form = HistoriaChefe()
+    return render(request, 'usuarios/chefe_form.html')
