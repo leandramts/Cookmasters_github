006924f04f7b52f22f895b700b6a4dcbe4f1a214
leandramts <leandra.mts10@gmail.com>
2025-11-22 16:20:37 -0400

    Esboço de um script

--- cookmasters_website/settings.py
+++ cookmasters_website/settings.py
@@ -104,13 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'America/Manaus'
 
 USE_I18N = True
 
 USE_TZ = True
+
+DEFAULT_CHARSET = 'utf-8'
 
 
 # Static files (CSS, JavaScript, Images)
